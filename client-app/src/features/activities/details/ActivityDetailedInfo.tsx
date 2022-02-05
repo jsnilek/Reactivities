d766a17a369e75474a6a3fb125408dd063cc52ea
jan.jilek <jan.jilek@alteryx.com>
2022-02-05 21:14:48 +0100

    end of section 11

--- client-app/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -1,6 +1,7 @@
 ﻿
 import { observer } from 'mobx-react-lite';
 import React from 'react'
+import {format} from 'date-fns'
 import {Segment, Grid, Icon} from 'semantic-ui-react'
 import {Activity} from "../../../app/models/activity";
 
@@ -28,7 +29,7 @@ export default observer(function ActivityDetailedInfo({activity}: Props) {
                     </Grid.Column>
                     <Grid.Column width={15}>
             <span>
-              {activity.date}
+              {format(activity.date!, 'dd MMM yyyy h:mm aa')}
             </span>
                     </Grid.Column>
                 </Grid>
